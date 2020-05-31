8931c7694ef85a1a78a00892336f0546f48133a1
Vladarc <vladmironecdonnu@gmail.com>
2020-05-31 21:00:13 +0300

    merge sum and subtraction func , merge mult and division func

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@
 
 const aboutCar = {
 
-    manufacturer:"United States",
-    model:'BMW',
-    year:2010,
-    speedAvg:60,
+    manufacturer: "United States",
+    model: 'BMW',
+    year: 2010,
+    speedAvg: 60,
 
-    show(){
+    show() {
 
         let display = `
          <ul style="list-style:none"> 
@@ -21,19 +21,19 @@ const aboutCar = {
          `;
         document.write(display);
     },
-    caclTime(distance = 500,speed = this.speedAvg) {
-   
-        
-        if( 0 == distance || speed == 0 ){
+    caclTime(distance = 500, speed = this.speedAvg) {
+
+
+        if (0 == distance || speed == 0) {
             document.write('Значения не валидны ');
-        }  
-        
+        }
+
         let time = distance / speed;
-        let tmp  = Math.floor(time / 4);
+        let tmp = Math.floor(time / 4);
 
         let out = `
         Необходимое время для преодоления расстояния: ${distance} км, необходимо ${Math.floor(time + tmp)} часов `;
-        
+
         document.write(out);
 
 
@@ -55,225 +55,202 @@ const aboutCar = {
 // 2. Создать объект, хранящий в себе отдельно числитель и знаменатель дроби, и следующие функции для работы с этим объектом: 
 
 const firstFraction = {
-    numerator:15,
-    denominator:9
+    numerator: 15,
+    denominator: 9
 }
 const sectondaryFraction = {
-    numerator:12,
-    denominator:4
+    numerator: 12,
+    denominator: 4
 }
 
 
 
 
-const nod = ( a, b ) => {
-    if ( ! b ) {
+const nod = (a, b) => {
+    if (!b) {
         return a;
     }
-    return nod( b, a % b );
-};   
+    return nod(b, a % b);
+};
 
-const sum = ( fractionOne= {},fractionTwo = {} ) => {
+const isValid = (ob1, ob2) => {
+    if (Object.keys(ob1).length == 0 ||
+        Object.keys(ob2).length == 0) {
+        return false
+    }
+    return true
+}
 
-    if( Object.keys( fractionOne ).length == 0  ||
-        Object.keys( fractionTwo ).length == 0  ) {
-           return "Don't valid value"
-       }
+
+
+//  сложенние и вычитание 
+const sumAndSubtraction = (fractionOne = {}, fractionTwo = {},operator) => {
+
+    if (!isValid(fractionOne, fractionTwo)) {
+        return "Isn't valid value"
+    }
+
+    const { numerator: numObjOne, denominator: denObjOne } = fractionOne;
+
+    const { numerator: numObjTwo, denominator: denObjTwo } = fractionTwo;
+
+
+    let denominator         = 0,
+        firstNumeratorCalc  = 0,
+        secondNumenatorCalc = 0,
+        result              = 0
        
-       const  {numerator:numObjOne,denominator:denObjOne} = fractionOne;
 
-       const  {numerator:numObjTwo,denominator:denObjTwo} = fractionTwo;
-   
-
-       let denominator                  = 0,
-       out                              = null,
-       firstNumerator                   = 0,
-       calc                             = 0
-
-       denominator    =       denObjOne * denObjTwo;
-       firstNumerator =  ( numObjOne *  denObjTwo ) + ( numObjTwo * denObjOne );
-       calc           =  nod(firstNumerator,denominator)
-       out            = ` ${firstNumerator / calc} / ${denominator / calc} `
-                        
-       return    out      
-
-    }      
-    
-    console.log(sum( firstFraction,sectondaryFraction ))    
-    // console.log( sum( {},{} ) )    
-
-
-
-
-
-              
-                       
- 
-    
-
-                      
-    
-const subtraction = ( fractionOne = {},fractionTwo = {} ) => {
-
-    if(Object.keys( fractionOne ).length == 0  ||
-       Object.keys( fractionTwo ).length == 0  ) {
-           return "Don't valid value"
-       }
-   
-
-    const  {numerator:numObjOne,denominator:denObjOne} = fractionOne;
-
-    const  {numerator:numObjTwo,denominator:denObjTwo} = fractionTwo;
-
-    let firstNumeratorCalc  =  0,
-        secondNumenatorCalc =  0,
-        denominator         =  0,
-        result              =  0,
-        out                 = null
-
-
-
-        firstNumeratorCalc      =  numObjOne * denObjTwo
-        secondNumenatorCalc     =  numObjTwo * denObjOne
-        denominator             =  denObjOne * denObjTwo
-        result                  = firstNumeratorCalc - secondNumenatorCalc
-        
-            
-
-        if( Math.sign( result ) == -1 ){
-            out = ` - ${Math.abs(result)} / ${denominator}`
-          
-          
-            return out     
-           
+        if( operator === "+" || operator === "plus" ){
+        denominator = denObjOne * denObjTwo;
+        firstNumeratorCalc = (numObjOne * denObjTwo) + (numObjTwo * denObjOne);
+        let calc = nod(firstNumeratorCalc, denominator)
+        result = ` ${firstNumeratorCalc / calc} / ${denominator / calc} `
         } else {
-            out = ` ${result} / ${denominator}`
            
-            return out
-        }   
-    }          
+            firstNumeratorCalc  = numObjOne * denObjTwo
+            secondNumenatorCalc = numObjTwo * denObjOne
+            denominator         = denObjOne * denObjTwo
+            result              = firstNumeratorCalc - secondNumenatorCalc
+            Math.sign(result) == -1 ? result = ` - ${Math.abs(result)} / ${denominator}` : 
+                result = ` ${result} / ${denominator}`                 
+            
+        }
     
-// console.log(subtraction(firstFraction,sectondaryFraction))
-// console.log(subtraction({},{}))
 
 
-const multiplication = ( fractionOne = {},fractionTwo = {} ) => {
-   
-    if(Object.keys(fractionOne).length == 0  ||
-       Object.keys(fractionTwo).length == 0  ) {
-        return "Don't valid value"
-    }
 
-    const  {numerator:numObjOne,denominator:denObjOne} = fractionOne;
 
-    const  {numerator:numObjTwo,denominator:denObjTwo} = fractionTwo;
 
-    let numerator  =  numObjOne * numObjTwo,
-    denominator    =  denObjOne * denObjTwo,
-    result         =  `${numerator} / ${denominator}`
-    
-    return result 
-
-}
-
-console.log( multiplication( firstFraction,sectondaryFraction ) )     
- 
-
-const division  = ( fractionOne = {},fractionTwo = {} ) => {
-
-    if(Object.keys( fractionOne ).length == 0  ||
-    Object.keys( fractionTwo ).length == 0  ) {
-     return "Don't valid value"
-    }
-
-    const  {numerator:numObjOne,denominator:denObjOne} = fractionOne;
-
-    const  {numerator:numObjTwo,denominator:denObjTwo} = fractionTwo;
-
-    let numerator = numObjOne * denObjTwo,
-    denominator   = numObjTwo * denObjOne,
-    result        = `${numerator} / ${denominator}`
-
-    return result
-} 
-console.log( division( firstFraction,sectondaryFraction ) )
-
-const reduction = ( obj )=> {
-    if( Object.keys(obj).length == 0 ) {
-       return "Don't valid value"
-    }
-    
-    const  {numerator:numObjOne,denominator:denObjOne} = obj;
-
-    let reducing = nod( numObjOne,denObjOne ),
-    result       = `${numObjOne / reducing} / ${denObjOne / reducing}`
 
 
     return result
-    
+
 }
+//  Результаты 
+console.log("Сумма дробей",sumAndSubtraction(firstFraction, sectondaryFraction,"+"))
+console.log("Вычитание дробей",sumAndSubtraction(firstFraction, sectondaryFraction,"-"))
+// console.log( sum( {},{} ) )    
 
-  console.log( reduction( firstFraction ) )          
+
+// Умножение и Деление
+const multiplicationAndDivision = (fractionOne = {}, fractionTwo = {},operator) => {
+
+    if (!isValid(fractionOne, fractionTwo)) {
+        return "Isn't valid value"
+    }
+
+    const { numerator: numObjOne, denominator: denObjOne } = fractionOne;
+
+    const { numerator: numObjTwo, denominator: denObjTwo } = fractionTwo;
+
+    let numerator      = 0,
+        denominator    = 0,
+         result        = 0
+
+    if( operator === '*' || operator === "mult" ){
         
+        numerator = numObjOne * numObjTwo,
+        denominator = denObjOne * denObjTwo,
+        result = `${numerator} / ${denominator}`
+    } else {
+
+        numerator = numObjOne * denObjTwo,
+        denominator = numObjTwo * denObjOne,
+        result = `${numerator} / ${denominator}`
+
+     }
+
+
+
+
+
+    return result
+
+}
+//  Результаты 
+console.log("Умножение дробей",multiplicationAndDivision(firstFraction,sectondaryFraction,'*'))
+console.log("Деление дробей",multiplicationAndDivision(firstFraction,sectondaryFraction))
+
+
+
+// cокращение дробей
+const reduction = (obj) => {
+    if (Object.keys(obj).length == 0) {
     
+        return "Isn't valid value"
+    }
+
+    const { numerator: numObjOne, denominator: denObjOne } = obj;
+
+    let reducing = nod(numObjOne, denObjOne),
+        result = `${numObjOne / reducing} / ${denObjOne / reducing}`
 
 
-    
+    return result
 
- 
- 
-
-
-
-
-
-  
-
-
-        
-   
-            
-        
-      
-
-            
-            
-    
-    
-
-
- 
-
-       
-         
-         
-           
-         
-
-        
-       
-   
-        
-        
-
-       
-        
-       
-       
-       
-    
-
-
-
-
-     
-
-    
+}
+//  Результаты 
+console.log("Сокращение дроби",reduction(firstFraction))
 
 
 
 
 
 
-    
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
